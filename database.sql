34d12ada9fe0c445c20cf1636b0b940dc2ce4dce
Iaroslav <GunGalla@gmail.com>
2023-03-07 14:39:06 +0200

    Tests

--- database.sql
+++ database.sql
@@ -1,6 +1,5 @@
-DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS urls, url_checks;
 DROP TABLE IF EXISTS url_checks;
-
 CREATE TABLE urls (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name varchar(255) UNIQUE,
